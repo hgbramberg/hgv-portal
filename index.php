e0a3c47ec81f2bc997370b1c74f9b1e7d235d792
Yves Ledermann <yves@laedis.ch>
2017-10-12 22:21:34 +0200

    now with dynamic submenu, errorpage anf check if includepage is file

--- index.php
+++ index.php
@@ -1,10 +1,153 @@
 <!DOCTYPE html>
 <?php 
-        // Save club name during session
+	// Save club name during session
         session_start();
         if (isset ($_GET['club']))
         $_SESSION['club'] = $_GET['club'];
         $club = $_SESSION['club'];
+
+	// Save page name
+		if (isset ($_GET['page']))
+		$page = $_GET['page'];
+
+	// Functions for Submenu generation
+		function curPageURL() {
+			 $pageURL = '';
+
+			 if ($_SERVER["SERVER_PORT"] != "80") {
+			  $pageURL .= $_SERVER["REQUEST_URI"];
+			 } else {
+			  $pageURL .= $_SERVER["REQUEST_URI"];
+			 }
+			 return $pageURL;
+			}
+
+		function ordered_list($array,$parent_id = 0)
+			{
+			  $temp_array = array();
+			  foreach($array as $element)
+			  {
+				if ($element['parent_id'] == $parent_id)
+				{
+				  $element['subs'] = $ordered_list($array, $element['id']);
+				  $temp_array[] = $element;
+				}
+			  }
+			  return $temp_array;
+			}
+
+		function bootstrap_menu($array,$parent_id = 0,$parents = array()) {
+				if($parent_id==0)
+				{
+					foreach ($array as $element) {
+						if (($element['parent_id'] != 0) && !in_array($element['parent_id'],$parents)) {
+							$parents[] = $element['parent_id'];
+						}
+					}
+				}
+				$menu_html = '';
+				foreach($array as $element)
+				{
+
+					if($element['parent_id']==$parent_id)
+					{
+						if(in_array($element['id'],$parents))
+						{
+
+
+							$menu_html .= '<li class="dropdown">';
+							$menu_html .= '<a href="'.$element['url'].'" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">'.$element['name'].' <span class="caret"></span></a>';
+						}
+						else   {
+						 $pagenameurl=$element['url'];
+						 $pagename= curPageURL();
+						 $host =$_SERVER['REQUEST_URI'];
+						 $pages = 'home.php';
+
+
+						if($host==$pagenameurl){  $act="active";} else { $act=""; }
+							$menu_html .= '<li class='.$act.'>';
+                            $menu_html .= '<a href="' . $element['url'] . '">' . $element['name'] . '</a>';
+						}
+						if(in_array($element['id'],$parents))
+						{
+							$menu_html .= '<ul class="dropdown-menu" role="menu">';
+							$menu_html .= bootstrap_menu($array, $element['id'], $parents);
+							$menu_html .= '</ul>';
+						}
+						$menu_html .= '</li>';
+					}
+				}
+				return $menu_html;
+			}
+
+// the Menu Array
+		$menu_items = array(
+		array(
+			"id" => "1", 
+			"url" => '?page=home.php', 
+			"parent_id" => "0", 
+			"name" => 'Home', 
+			"order" => "0",
+
+		),
+		array(
+			"id" => "2", 
+			"url" => '?page=pages/spiele.php', 
+			"parent_id" => "0", 
+			"name" => 'Agenda', 
+			"order" => "20",
+
+		), 
+		array(
+			"id" => "20", 
+			"url" => '?page=pages/spieleMitResultatTingle.php', 
+			"parent_id" => "0", 
+			"name" => 'Spiele', 
+			"order" => "20",
+
+		), 
+		array(
+			"id" => "300", 
+			"url" => '', 
+			"parent_id" => "0", 
+			"name" => 'Durchschnitt', 
+			"order" => "0",
+
+		), 
+		array(
+			"id" => "310", 
+			"url" => '?page=pages/schnitt.php', 
+			"parent_id" => "300", 
+			"name" => 'Durchschnitt', 
+			"order" => "0",
+
+		), 
+		array(
+			"id" => "320", 
+			"url" => '?page=pages/schnittsaison.php', 
+			"parent_id" => "300", 
+			"name" => 'Saisonverlauf', 
+			"order" => "10",
+
+		),  
+		array(
+			"id" => "330", 
+			"url" => '?page=pages/schnittentwicklung.php', 
+			"parent_id" => "300", 
+			"name" => 'letze 5 Jahre', 
+			"order" => "10",
+
+		),  
+		array(
+			"id" => "400", 
+			"url" => '?page=pages/rangpunkte.php', 
+			"parent_id" => "0", 
+			"name" => 'Rangpunkte', 
+			"order" => "20",
+
+		)
+	);
 ?>
 <html lang="en">
 <head>
@@ -39,16 +182,6 @@
 </head>
 <body>
 
-<?php
-// TopBar Menu defines
-$menuitems = array(
-  'Spiellisten'     => array('text'=>'Spiele',          'url'=>'spieleMitResultatTingle.php'),
-  'Durchschnitt'    => array('text'=>'Durchschnitt',    'url'=>'schnittsaison.php'),
-  'Rangpunkte'      => array('text'=>'Rangpunkte',      'url'=>'rangpunkte.php'),
-  'Streiche'        => array('text'=>'Streiche',        'url'=>'schnitt.php'),
-);
-?>
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
   <div class="navbar-header">
@@ -62,37 +195,29 @@ $menuitems = array(
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <?php
-        // Dynamic Menu
-        foreach ($menuitems as $menuitem)
-        {
-            if (isset($_GET['page']) && $_GET['page'] == $menuitem['url'])
-            {
-                echo '<li><a href="?page=' . $menuitem['url'] . '" class="active"> ' . $menuitem['text'] . '</a></li>';
-              
-                $activePage = $menuitem['url'];
-            }
-            else
-            {
-                echo '<li><a href="?page=' . $menuitem['url'] . '"> ' . $menuitem['text'] . '</a></li>';
-            }
-        }
+        // Show the dynamic Menu
+        $top_menu = bootstrap_menu($menu_items);
+        echo $top_menu;
         ?>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="https://www.hgverwaltung.ch" target="_blank"> <span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
+      </ul> 
     </div>
   </div>
 </nav>
   
 <div class="container-fluid text-center">    
+	 <?php echo "DEBUG: Übergeben wurde die activePage ". ($_GET['page']); ?>
   <div class="row content">
     <div class="col-sm-10 text-left"> 
      <?php 
-        // Include page
-        if (isset($activePage))
-        {
-            include $activePage;   
+        // Include page if set		
+		if (isset ($page))
+		{
+			// check if page name ist file
+			if (is_file($page)) {
+				include ($page);
+			} else {
+				include "error.php";
+			}		   
         }
         else
         {
@@ -104,5 +229,32 @@ $menuitems = array(
   </div>
 </div>
 
+	  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>
+$(document).ready(function($){
+    var url = window.location.href;
+    $('.nav li a[href="'+url+'"]').addClass('active');
+});
+</script>
+<script>
+
+
+$(function(){
+
+    var url = window.location.pathname, 
+        urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+        // now grab every link from the navigation
+        $('.nav li a').each(function(){
+            // and test its normalized href against the url pathname regexp
+            if(urlRegExp.test(this.href.replace(/\/$/,''))){
+                $(this).addClass('active');
+            }
+        });
+
+});
+</script>
+	
+	
 </body>
 </html>
