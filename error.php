65f8adb3673e290759277f90d17cae591bb657cc
Yves Ledermann <yves@laedis.ch>
2017-10-12 22:20:16 +0200

    added error page with some debug outputs

---
+++ error.php
@@ -0,0 +1,7 @@
+    <h1>Fehler</h1>
+   <div>Hoppla es ist ein Fehler aufgetreten!</div>
+   
+    
+    
+    <p><?php echo "Übergeben wurde der Club $club"; ?></p>
+    <p><?php echo "Übergeben wurde die Page $page"; ?></p>
