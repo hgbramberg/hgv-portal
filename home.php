84d22ffd670bb60357e5f74ffe3c6b3f87c03835
Yves Ledermann <yves@laedis.ch>
2018-12-23 21:56:23 +0100

    rename

--- home.php
+++
@@ -1,5 +0,0 @@
-    <h1>Statistiken von der HG Verwaltung</h1>
-    
-    <p>Enjoy =)</p>
-
-    
--- pages/changelog.php
+++
@@ -1,9 +0,0 @@
-<?php echo shell_exec('git log ?'); ?>
-
-<?php
-    // Anzeige der CHANGELOG.md
-    require "includes/parsedown/Parsedown.php";
-    $file = file_get_contents('CHANGELOG.md');
-    $Parsedown = new Parsedown();
-    echo $Parsedown->text($file);
-    ?>
